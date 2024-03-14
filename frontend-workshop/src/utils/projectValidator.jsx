6efba0d4915b475c50a5b02b4cd7d1ece1f970a4
leandrogrng <leandro.garing@gmail.com>
2024-03-14 10:46:45 +0800

    Added a Project Validator

---
+++ frontend-workshop/src/utils/projectValidator.jsx
@@ -0,0 +1,35 @@
+const validProjectName = (data) => {
+    const regexPName = /^[a-zA-Z]+(?:\s[a-zA-Z]+)*$/
+    return regexPName.test(data?.name);   
+}
+
+const validProjectDescription = (data) => {
+    return /\s+/.test(data?.description);
+}
+
+const validAlias = (data) => {
+    const regexAlias = /^[a-z][A-Za-z]*$/;
+    return regexAlias.test(data?.alias);
+}
+
+export const validateProject = (data) => {
+    const retData = {isValid: true, errors: {}}
+
+    if(!validProjectName({name: data?.name})) {
+        retData.isValid = false;
+        retData.errors.name = 'Invalid project name'
+    }
+
+    if(!validProjectDescription({description: data?.description})) {
+        retData.isValid = false;
+        retData.errors.description = 'Description required'
+    }
+
+    if(!validAlias({alias: data?.alias})) {
+        retData.isValid = false;
+        retData.errors.alias = 'Invalid alias. Should not contain any spaces.'
+    }
+
+    return retData;
+};
+export default validateProject;

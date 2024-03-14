e68b1eb8d6b62a8e0c42b5f3417093ce2e1e6182
leandrogrng <leandro.garing@gmail.com>
2024-03-14 10:48:10 +0800

    Edited pathway in Link tag

--- frontend-workshop/src/App.jsx
+++ frontend-workshop/src/App.jsx
@@ -1,7 +1,6 @@
 import {Box, ChakraProvider, Flex, Stack} from '@chakra-ui/react'
 import { Link, Outlet, useLocation } from 'react-router-dom'
 import theme from './theme'
-import mockApi from './utils/mockApi';
 
 function App() {
   const {pathname} = useLocation();
@@ -14,13 +13,13 @@ function App() {
             <Link to="/" 
                   className={pathname === '/' ? 'menuActive' : 'menu'}> Home </Link>
             <Link to="/resources" 
-                  className={pathname === '/resources' ? 'menuActive' : 'menu'}> Resources </Link>
+                  className={pathname.startsWith('/resources') ? 'menuActive' : 'menu'}> Resources </Link>
             <Link to="/projects" 
-                  className={pathname === '/projects' ? 'menuActive' : 'menu'}> Projects </Link>
+                  className={pathname.startsWith('/projects') ? 'menuActive' : 'menu'}> Projects </Link>
             <Link to="/companies" 
-                  className={pathname === '/companies' ? 'menuActive' : 'menu'}> Companies </Link>
+                  className={pathname.startsWith('/companies') ? 'menuActive' : 'menu'}> Companies </Link>
             <Link to="/requests" 
-                  className={pathname === '/404' ? 'menuActive' : 'menu'}>Requests</Link>
+                  className={pathname.startsWith('/404') ? 'menuActive' : 'menu'}>Requests</Link>
           </Stack>
         </Box>
         <Box id='outlet'>
@@ -30,33 +29,5 @@ function App() {
     </ChakraProvider>
   );
 }
-
-/*
-function App() {
-  return (
-    <>
-      <ChakraProvider id= 'main'>
-        <div className = 'root'>
-          <div id= 'sidebar'>
-            <Stack>
-              <Link className='menu' to = "/"> Home </Link>
-              <Link className='menu' to = '/resources'> Resources </Link>
-              <Link className='menu' to = '/projects'> Projects </Link>
-              <Link className='menu' to = '/companies'> Companies </Link>
-              <Link className='menu' to = '/requests'> Requests</Link>    
-              <h6> Copyright 2024</h6>          
-            </Stack>
-          </div>
-
-          <div id= 'display'>
-            <Outlet/>
-          </div>
-        </div>
-      
-      </ChakraProvider>
-    </>
-  )
-}
-*/
 
 export default App

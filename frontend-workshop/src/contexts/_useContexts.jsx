30e22a2d1c2c6c932d6f32d0c173116e11b3c9b0
leandrogrng <leandro.garing@gmail.com>
2024-03-15 12:09:49 +0800

    Revert "Added all pages in the useContext file"
    This reverts commit 1460416b9c8824a37fdea05e49de095361f1df90.

--- frontend-workshop/src/contexts/_useContexts.jsx
+++ frontend-workshop/src/contexts/_useContexts.jsx
@@ -1,9 +1,6 @@
 import { useContext } from 'react';
 import { CompanyContext } from './Company';
-import { ResourceContext } from './Resources';
 
 const useCompany = () => useContext(CompanyContext);
-const useProject = () => useContext(ProjectContext);
-const useResources = () => useContext(ResourceContext);
 
-export { useCompany, useProject, useResources };
+export { useCompany };

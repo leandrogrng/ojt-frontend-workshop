e895f36b9b93450d728097152b779ff7ca1ac198
leandrogrng <leandro.garing@gmail.com>
2024-03-18 06:03:22 +0800

    Revert the responsiveness

--- frontend-workshop/src/App.jsx
+++ frontend-workshop/src/App.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
-import { ChakraProvider, Grid, GridItem } from '@chakra-ui/react';
-import { Outlet } from 'react-router-dom';
+import { Box, ChakraProvider, Flex, Grid, GridItem, Stack } from '@chakra-ui/react';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import theme from './theme';
 import SideNav from './components/SideNav';
 
 function App() {
+  const{pathname} = useLocation();
   return (
     <ChakraProvider theme={theme}>
-      <Grid w='full'
-            templateAreas= {{
-              base: `"sidenav"
-                    "content"`,
-              md: `"sidenav content"`
-            }}
-            gridTemplateColumns={[`1fr`, `1fr`,`200px 1fr`]}>
-        <GridItem area='sidenav' id='sidebar'>
-          <SideNav/>
-
-        </GridItem>
-        <GridItem area='content'>
-          <Outlet/>
-
-        </GridItem>
-      </Grid>
+      <Flex>
+        <Box id = 'sidebar'>
+          <Box id = 'imageContainer'/>
+          <Stack spacing={0} pt={30}>
+            <Link to="/" 
+                  className={pathname === '/' ? 'menuActive' : 'menu'}> Home </Link>
+            <Link to="/resources" 
+                  className={pathname === '/resources' ? 'menuActive' : 'menu'}> Resources </Link>
+            <Link to="/projects" 
+                  className={pathname === '/projects' ? 'menuActive' : 'menu'}> Projects </Link>
+            <Link to="/companies" 
+                  className={pathname === '/companies' ? 'menuActive' : 'menu'}> Companies </Link>
+            <Link to="/requests" 
+                  className={pathname === '/404' ? 'menuActive' : 'menu'}>Requests</Link>
+          </Stack>
+        </Box>
+        <Box id='outlet'>
+          <Outlet />
+        </Box>
+      </Flex>      
     </ChakraProvider>
   );
 }

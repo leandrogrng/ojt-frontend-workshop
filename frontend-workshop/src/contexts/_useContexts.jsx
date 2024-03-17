f04ccde8b24c39c9b1830f669bb80daba40f6ab8
leandrogrng <leandro.garing@gmail.com>
2024-03-18 05:52:34 +0800

    Added all Contexts

--- frontend-workshop/src/contexts/_useContexts.jsx
+++ frontend-workshop/src/contexts/_useContexts.jsx
@@ -1,6 +1,10 @@
 import { useContext } from 'react';
 import { CompanyContext } from './Company';
+import { ResourceContext } from './Resources';
+import { ProjectsContext } from './Projects';
 
 const useCompany = () => useContext(CompanyContext);
+const useProject = () => useContext(ProjectsContext);
+const useResources = () => useContext(ResourceContext);
 
-export { useCompany };
+export { useCompany, useProject, useResources };
